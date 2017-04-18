92b444b3b162a5ce64dc03ca558149e0036b7371
c1ay <c1ay@c1ay.cc>
2017-04-18 15:15:18 +0800

    web

---
+++ web/app/main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Hello from '../component';
+
+main();
+
+function main() {
+    React.render(<Hello />, document.getElementById('app'));
+}
---
+++ web/webpack.config.js
@@ -0,0 +1,16 @@
+var path = require('path')
+
+module.exports = {
+    entry: [
+        path.resolve(__dirname, 'app/main.js')
+    ],
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+          { test: /\.js|jsx$/, exclude: /node_modules/, loader: 'babel-loader', query: { presets: ['react', 'es2015']}}
+        ]
+    },
+}
